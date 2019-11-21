7410962355af4dbf36abf8fd24978adc8eb53810
GALEN STEVENSON <galenstevenson1@gmail.com>
2019-11-21 09:54:51 -0700

    updated README and Week12

---
+++ README.md
@@ -0,0 +1,7 @@
+"""
+GALEN STEVENSON
+CSCI 102 C
+WEEK 12 LAB A
+"""
+
+This is the readme for week 12.  This is a basic Git tutorial and function exercise in Python....
---
+++ Week12-utility.py
@@ -0,0 +1,31 @@
+"""
+GALEN STEVENSON
+CSCI 102 -C
+WEEK 11 LABA
+"""
+
+# PRINT OUTPUT
+def PrintOutput(string):
+    print("OUTPUT", string)
+    
+# LOAD FILE
+def LoadFile():
+
+# UPDATE STRING
+def UpdateString():
+
+# FIND WORD COUNT
+def FindWordCount():
+
+#SCORE FINDER
+def ScoreFinder():
+
+# UNION
+def Union():
+
+# INTERSECTION
+def Intersection():
+
+# NOT IN
+def NotIn():
+    
