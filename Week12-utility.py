34069f3befdd987d312808a9e1d777ab6fccfefd
GALEN STEVENSON <galenstevenson1@gmail.com>
2019-11-21 23:27:40 -0700

    updated aaaggaaaiiiinnnnnn

--- Week12-utility.py
+++ Week12-utility.py
@@ -9,31 +9,62 @@ def PrintOutput(string):
     print("OUTPUT", string)
     
 # LOAD FILE
-def LoadFile():
+def LoadFile(file):
     f = open(file, "r")
+    f.read()
+    lines = []
+    for line in f:
+        lines.append(line.rstrip("\n")
+    
 
 # UPDATE STRING
-def UpdateString():
-    makin' changes like tacos or burritos
-    
+def UpdateString(string, string_add, num):
+    string_mod = ""
+    for i in range(len(string1)):
+        if i == num:
+            string_mod += string_add
+        else:
+            string_mod += string[i]
+        
 # FIND WORD COUNT
-def FindWordCount():
-    words and stuff and things
-
+def FindWordCount(mylist, string):
+    num = 0
+    for s in mylist:
+        s = s.split()
+        for word in s:
+            if word == string:
+                num += 1
+    
 #SCORE FINDER
-def ScoreFinder():
-    wats da score?
-
+def ScoreFinder(onelist, twolist, string):
+    scores = []
+    for p in onelist:
+        scores.append(p.lower())
+    string = string.lower()
+    if string in scores:
+        i = scores.index(string)
+        print("OUTPUT", twolist[i])
+    else:
+        print("OUTPUT player not found")
+    
 # UNION
-def Union():
-    who remembers union underground?  NO?  that's fine
+def Union(onelist, twolist):
+    biglist = onelist + twolist
+    return biglist
 
 # INTERSECTION
-def Intersection():
-    intersexion... perv
+def Intersection(onelist, twolist):
+    biglist = []
+    for v in onelist:
+        if v in twolist:
+            biglist.append(v)
+    return biglist
 
 # NOT IN
-def NotIn():
-    is it in yet?  Yer still perv.
-    
-#ONE MORE COMMIT FOR THE WIN    
+def NotIn(onelist, twolist):
+    biglist = []
+    for v in onelist:
+        if v not in twolist:
+            biglist.append(v)
+    return biglist
+
